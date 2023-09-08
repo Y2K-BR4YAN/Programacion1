915125cf88413dc670d4a8bdeb06b6d675cd7c7d
Y2K-BR4YAN <143621993+Y2K-BR4YAN@users.noreply.github.com>
2023-09-08 08:50:40 -0600

    Add files via upload

---
+++ AreaTriangulo/src/areatriangulo/AreaTriangulo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package areatriangulo;
+
+/**
+ *
+ * @author Y2K-BR4YAN
+ */
+public class AreaTriangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //numeros enteros
+        //numeros decimales
+    int A=15;
+    int base=17;
+    int area = A * base/2;
+    System.out.println("El area es:" + area);
+    
+    }
+    
+}
+//
